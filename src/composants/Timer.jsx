f715751ea4f5a0710abbd42f20a5923e145c4bfb
rine <sirine.elalami.22578@stu.he-ferrer.eu>
2025-10-07 14:09:44 +0200

    ex futur

--- src/composants/Timer.jsx
+++ src/composants/Timer.jsx
@@ -3,15 +3,25 @@ import React from 'react'
 class Timer extends React.Component {
     constructor(props){
         super(props)
-
         this.state = {
             date: new Date().toLocaleDateString(),
             heure: new Date().toLocaleTimeString()
-        };
+        }
+
+    }
+
+    componentDidMount(){
+        setInterval(maj, 3000)
+    }
+
+    componentWillUnmount(){
+            clearInterval()
     }
 
     render() {
-        return <p>Date: {this.state.date}, heure: {this.state.heure}</p>
+        return (
+            <p>Date: {this.state.date}, heure: {this.state.heure}</p>
+        )
     }
 }
 
